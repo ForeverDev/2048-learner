d7cbfe876234fc71583868245261fba4b526c639
ForeverDev <david.wilson@govsacademy.org>
2015-12-05 01:38:41 -0500

    added game.lua

---
+++ game.lua
@@ -0,0 +1,22 @@
+local Game = {
+  MOVEDIR = {
+    LEFT = 1;
+    RIGHT = 2;
+    UP = 3;
+    DOWN = 4;
+  }
+}
+
+function Game.new()
+
+  local self = {}
+
+  function self.RegisterMove(direction)
+
+  end
+
+  return self
+
+end
+
+return Game
